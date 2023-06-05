0d3fddc8d09d1c8be14c11459d4a11b01a0a7cdc
JyotiPi <jyotichetry087@gmail.com>
2023-06-05 14:35:05 +0530

    minor updates to code

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
-import requests
-from servo_motor import handleServo
-import RPi.GPIO as GPIO
-import time
-from pir_sensor import detect_motion
+from secret_keys import THINGSPEAK_CHANNEL_ID, THINGSPEAK_READ_API_KEY
 from capture_and_upload import capture_and_upload_image
-from secret_keys import THINGSPEAK_CHANNEL_ID,THINGSPEAK_READ_API_KEY 
-
+from pir_sensor import detect_motion
+from servo_motor import handleServo
+import time
+import RPi.GPIO as GPIO
+import requests
+print("------Main code running-----")
+GPIO.setmode(GPIO.BCM)
 
 servo_state_handler_url = f'https://api.thingspeak.com/channels/{THINGSPEAK_CHANNEL_ID}/feeds/last.json?api_key={THINGSPEAK_READ_API_KEY}'
 
 
-#currently facing problem of functions below running before above
+# currently facing problem of functions below running before above
 
 def main():
-    print("------Main code running-----")
     try:
         while True:
             response = requests.get(servo_state_handler_url)
@@ -24,18 +24,30 @@ def main():
                 servo_control_data = int(data['field1'])
 
             # Set the servo state based on the servo control value
+            print("servo_control_data:",servo_control_data)
             if servo_control_data == 1:
                 # api calls
                 print('api calls...')
+                print('feeding your pet...')
                 handle_servo_motion()
+                handle_servo_motion()
+                time.sleep(.5)
+                # capture_and_upload_image()
+                print('capturing image....')
+                time.sleep(6)
+                continue
 
             # calling the pir_sensor to detect object
             if detect_motion():
-                print('motion')
+                print('motion detected...')
+                print('feeding your pet....')
                 handle_servo_motion()
-                time.sleep(5)
-                capture_and_upload_image()
-                time.sleep(4)
+                handle_servo_motion()
+                time.sleep(.3)
+                print('capturing image....')
+                # capture_and_upload_image()
+                time.sleep(6)
+                continue
             else:
                 print("no motion")
 
@@ -44,12 +56,13 @@ def main():
 
 
 def handle_servo_motion():
-    print("on")
     handleServo("open")
-    print('off')
+    # print("feed on")
     handleServo("close")
+    # print('off')
 
 
 if __name__ == "__main__":
     GPIO.setwarnings(False)
+    handleServo('close')
     main()
--- pir_sensor.py
+++ pir_sensor.py
@@ -1,29 +1,31 @@
 import RPi.GPIO as GPIO
 import time
 
-sensor_pin = 36
-GPIO.setmode(GPIO.BOARD)
+sensor_pin = 26
+GPIO.setmode(GPIO.BCM)
 GPIO.setup(sensor_pin, GPIO.IN)
-print("Initializing PIR Sensor...")
-time.sleep(12)
-print("PIR Ready...")
 
 def detect_motion():
-
+    print('ir sensor ready...')
     if GPIO.input(sensor_pin):
-        # print(GPIO.input(sensor_pin))
-        # print("Motion Detected")
+        print(GPIO.input(sensor_pin))
+        print("Motion Detected")
+        while GPIO.input(sensor_pin):
+            time.sleep(.5)
+            print('waiting for a while...')
+
         return True
     else:
-        # print("just chilling...")
+        print("just waiting for a detection...")
         time.sleep(0.2)
         return False
 
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
 
-    motion_detected = detect_motion()
+#     while True:
+#         detect_motion()
 
 
 
